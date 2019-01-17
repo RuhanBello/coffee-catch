dfda39e34bbaa3cd44e724211bdcdd650fb12fde
Ruhan <ruhancvbello@gmail.com>
2019-01-17 04:44:03 -0200

    Aula 10: UI da tela de game over

--- Assets/Scripts/CoffeeBotBehaviour.cs
+++ Assets/Scripts/CoffeeBotBehaviour.cs
@@ -6,11 +6,14 @@ using UnityEngine.SceneManagement;
 public class CoffeeBotBehaviour : MonoBehaviour {
     public GameManager gameManager;
 
+    public GameObject gameOverScreen;
+
     private void OnCollisionEnter(Collision collision) {
         if (collision.gameObject.CompareTag("Player")) {
             TopDownController controller = collision.gameObject.GetComponent<TopDownController>();
             if (controller.coinsAmount >= 5) {
                 gameManager.GameOver();
+                gameOverScreen.SetActive(true);
             }
         }
     }
