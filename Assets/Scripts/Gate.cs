67362d1201f0313743599a377bd415efbd0d3e44
Ruhan <ruhancvbello@gmail.com>
2019-01-17 04:56:43 -0200

    Aula 12: corrotinas e acoes repetitivas

---
+++ Assets/Scripts/Gate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate : MonoBehaviour {
+    public GameObject lasers;
+
+    // Use this for initialization
+    private void Start() {
+        StartCoroutine(LasersCoroutine());
+    }
+
+    IEnumerator LasersCoroutine() {
+        while (true) {
+            lasers.SetActive(true);
+            yield return new WaitForSeconds(2);
+            lasers.SetActive(false);
+            yield return new WaitForSeconds(2);
+        }
+    }
+}
---
+++ Assets/Scripts/Lasers.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Lasers : MonoBehaviour {
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
